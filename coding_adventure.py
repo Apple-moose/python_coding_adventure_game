d799103ed32bd7988b6f74c42be7325f38e9d1ba
Apple-moose <patrissio@gmail.com>
2024-06-11 23:28:52 +0200

    Coding adventures 1st pitch

--- coding_adventure.py
+++ coding_adventure.py
@@ -12,8 +12,15 @@ def main():
  profile_zen = 0
  profile_angry = 0
  profile_smart = 0
- profile_exp = 0
  profile_obsess = 0
+ profile_dependent = 0
+
+ def profiling():
+     profiles = {'zen':profile_zen, 'angry':profile_angry, "smart":profile_smart, 'obsessed':profile_obsess, 'dependent':profile_dependent}
+     winner = max(profiles, key=profiles.get)
+    #  print(profiles)
+     print(analisis[winner])
+     
 
  def dice_throw(per):
         percentage = random.random() * 100
@@ -23,7 +30,6 @@ def main():
            return False 
         
  def obsession():
-     print("added to obsess profile: ",profile_obsess)
      if dice_throw(5):
         current_situation = 'success'
         print(situations[current_situation])
@@ -31,16 +37,61 @@ def main():
         current_situation = 'obsess'
         print("Still Nothing...Crap! continu y / n ?")
 
-    
- print("Welcome to the Coding Adventure Game!")
+ def sleeping():
+     if dice_throw(50):
+        current_situation = 'nap success'
+        print(situations[current_situation])
+     else:
+        current_situation = 'start'
+        print(situations["start"])
+
+ def cursing():
+     if dice_throw(20):
+        print(situations['angry endgame'])
+        exit()
+     else:
+        print("   !!! FU$%$😤    FU@^**#🤬    FU*%*@*#$^$#$%🤯   !!!")
+        print("")
+        print("(Double down with FUUU or feel BETTER now?)")
+
+ def slap():
+     if dice_throw(25):
+        print
+        print(situations['slap endgame'])
+        exit()
+     else:
+        print("...silence...")
+        print("")
+        print("You just realized that you could have hit some sensitive equipment. \
+Maybe not do that again?")
+        print("SLAP harder, explore more of the DARK side or come BACK to CODING?)")  
+
+ def hammer():
+     if dice_throw(50):
+        print(situations['angry endgame'])
+        exit()
+     else:
+        print("The wall still stands but it's getting weaker. Any moment....")
+        print("")
+        print("HAMMER further, explore more of the DARK side or feel BETTER now?)")
+
+ def explain():
+     if dice_throw(40):
+         print(situations['duck success'])
+     else:
+         print("You get into more and more details but nothing changes, you still find yourself blocked")
+         print("")
+         print("...and maybe a bit sad...EXPLAIN some more, check you OPTIONS or just STRANGLE the rubber duck")
+
+
+ print("Welcome to the 'A Coder's Rhapsody Game'!")
  input("Press <enter> to start...")
  print(" ")
  print("A plausible situational and psychological analisis")
- print("(Based on personal and shared experience)")
- input("<enter>...")
+ input("(Based on personal and shared experience)<enter>")
  print(" ")
- print("Please type 'get to work' when you feel ready to start!")
- print("(type 'help' in case you are blocked)")
+ print("Please type 'GET TO WORK' when you feel ready to start!")
+ print("(type 'HELP' in case you are blocked)")
 
  while True:
         command = input("-> ").lower()
@@ -51,9 +102,11 @@ def main():
             print(f"{situations[current_situation]}.")
         elif command == "study":
             current_situation = 'study'
+            profile_smart += 1
             print(situations["study"])
             input("time passes....<enter>")
             input("more time passes...")
+            print("")
             print("Although you feel like you are getting smarter by the minute, dinner time approaches \
 and you are still nowhere close to resolving your coding issues...")
             print("")
@@ -69,16 +122,119 @@ and you are still nowhere close to resolving your coding issues...")
             profile_obsess += 1
             obsession()
         elif command == "n":
+            profile_smart += 1
             current_situation = 'abandon'
             print(situations['abandon'])
+        elif command == "friend":
+            current_situation = 'call_friend'
+            profile_dependent += 1
+            # print('profile dependent added;', profile_dependent)
+            print(situations['call_friend'])
+            input("(Press <enter> to swallow some of your coder's pride...)")
+            print("")
+            current_situation = 'start'
+            input("The next day...")
+            print(situations['start'])
+        elif command == "chatgpt":
+            current_situation = 'chatgpt'
+            profile_dependent += 2
+            input("(Press <enter> and may the omnipotent A.I. one day rule the World!)")
+            print("")
+            print(situations['chatgpt'])
+        elif command == "sleep":
+            current_situation = 'sleep'
+            print(current_situation)
+            profile_smart += 1
+            print(situations['sleep'])
+            print("")
+            input('Press <enter> to wake up')
+            sleeping()
+        elif command == "scream":
+            profile_angry += 0.5
+            # print("state of angry profile: ", profile_angry)
+            input("(Press <enter> to scream in that pillow and cry it out a little...)")
+            current_situation = 'pillow'
+            print(situations['pillow'])
+        elif command == "f-word":
+            profile_angry += 1
+            # print("state of angry profile: ", profile_angry)
+            input("You breathe in all the air your lungs can admit and let out a magnificient angry FUUUUUUUUUUUU@#$% !!!<enter>")
+            current_situation = 'cursing'
+            print(situations[current_situation])
+        elif command == "fuuu":
+            profile_angry += 2
+            # print("state of angry profile: ", profile_angry)
+            input("(Fffffffff......)")
+            current_situation = 'cursing'
+            cursing()
+        elif command == "slap":
+            profile_angry += 1
+            current_situation = 'slap'
+            input("You raise your hand and hit <enter> the table in frustration...")
+            input("SHHLLPPLAAAAKKK!!!")
+            slap()
+        elif command == "hammer":
+            profile_angry += 3
+            current_situation = 'punch'
+            input("You grab the hammer with both hands and hit as hard as you can...<enter>")
+            input("KAAABOUUUUUMMMMM, KRRAAAKKK, Schling schlingg...")
+            hammer()
+        elif command == "explain":
+            profile_smart += 1
+            current_situation = 'duck'
+            input("You confess to the rubber duck...<enter>")
+            input("He seems rather perplexed...")
+            explain()
+        elif command == "chocolate":
+            profile_zen += 1
+            current_situation = 'kitchen'
+            input('the chocolate <enter> your watering mouth')
+            print("")
+            print("Mmmmmh, Delicious!")
+            print("")
+            print(situations['tea'])
+        elif command == "tea":
+            profile_zen += 1
+            current_situation = 'kitchen'
+            input('You press <enter> on the water boiler...')
+            print("")
+            print(situations['sandwich'])
+        elif command == "sandwich":
+            profile_zen += 2
+            current_situation = 'kitchen'
+            input('You insert all those wonderful ingredient between integral slices of bread...')
+            print("")
+            print("Best Sandwich Ever!")
+            print("")
+            print(situations['break'])
+        elif command == "break":
+            profile_zen += 2
+            current_situation = 'kitchen'
+            input('Press <enter> to check your phone...')
+            print("")
+            print("La la lah...ouh, what's up with that...")
+            print("")
+            print(situations['after break'])
+        elif command == "profile":
+            current_situation = "profile"
+            input('Please take a deep breath and press <enter> when you are ready')
+            print('')
+            profiling()
+            print("")
+            print("Keep on CODING, QUIT or EXIT this mini game")
+
         elif command == "help":
             print(" ")
             print("Try typing keywords from the text, such as: duck, fridge, stack overflow...")
-            print("...'quit' to end the game or 'score' to watch over your psychological profile! ")
+            print("...'exit' to end the game or 'score' to watch over your psychological profile! ")
         elif command == "quit":
+            print('👿 Yeah, just what I thought 😈')
+            break
+        elif command == "exit":
+            print('Thanks and hope to see you soon!')
             break
         else:
-            print("Say what???")
+            print("Say Whaaaat???🫣")
 
 if __name__ == "__main__":
     main()
--- game_data.py
+++ game_data.py
@@ -1,73 +1,131 @@
 situations = {
     "start": "So, here you are again, laptop in hands. You start to WRITE CODE",
-    "first options": "Your think that it's going to work this time, but you hit this \
-damn meanError again...What is it going to be this time? \
-You turn to STACK OVERFLOW, talk to the DUCK or walk to the FRIDGE? (you can always call a FRIEND for a quick fix)",
-    "stack overflow": "You turn to your most trusted friend on the web. There are tons of solutions \
-exposed on many pages but, somehow, it's all gibberish, nothing is usable...Be honest: you start building internal FRUSTRATION or \
-you calmly turn to other ressources and STUDY some more?",
-    "duck": "y",
-    "fridge": "y",
-    "friend": "After a couple of calls, finally someone picks up and helps you solve the issue. \
-Smart friend. Always there for you. Now, on to the next issue...(write code)",
-    "frustration": "y",
+    "first options": "You gather your focus and put your mind at work with confidence. Right away, your flow gets interrrupted by this damn 'meanError' again\
+...Time to make a decision: You turn to GOOGLE (STACK overflow, or chatGPT?), talk to the DUCK or walk to the KITCHEN? (you can always call a FRIEND for a quick fix)",
+    "stack overflow": "You end up scrolling again on Stack Overflow, your most trusted friend on the web. There are tons of solutions \
+exposed on many pages but, somehow, it's all gibberish, nothing is usable...At this point, be honest: you start building internal FRUSTRATION or \
+you calmly turn to other ressources and STUDY some more? Call a FRIEND? open CHATGPT? (you could always SLEEP on it...)",
+    "duck": "You look around and make eye contact with the rubber duck on your desk. You are well aware of the magic involved in \
+RUBBER DUCKING. The question is: do you really believe in it? The duck looks back at you faithfully, listening...What are you going to do: try to EXPLAIN your coding problems \
+to Donald or don't be a fool and re-consider your OPTIONS?",
+    "rubber ducking": "In software engineering, rubber duck debugging (or rubberducking) is a method of debugging code by articulating a \
+problem in spoken or written natural language. The name is a reference to a story in the book The Pragmatic Programmer in which a programmer \
+would carry around a rubber duck and debug their code by forcing themselves to explain it, line by line, to the duck (press B+A+C+K, in that order with a fingers \
+on the keyboard. Then with the same finger or another press <enter>...)",
+    "kitchen": "Let's get some air in the lungs and refill your energy levels. What's the rush anyways? You walk \
+to the kitchen slowly, thinking about what you will find there...You take the opportunity to drink a bit of water and \
+end up fighting yourself a little over that box on the counter with a big 'Merci' written on it... Steal some CHOCOLATE or \
+BACK to work?",
+    "tea": "You wipe away the brown from the corners of your smiling mouth and notice the water boiler. Now, how about preparing \
+yourself a nice cup of TEA? Maybe eat another piece of CHOCOLATE? ..or BACK to work for some reason...???",
+    "sandwich": "Waiting for the water to boil, you prepare your cup and check in the fridge for some cream. You \
+think about going BACK to work...also, you can't help but notice all the necessary ingredients to make yourself one of your favorite SANDWICH. Again: 'What's \
+the rush, right? Very tempting. It would sure take your belly out of the equation in your next coding session...",
+    "break": "There it is: true pleasure. A Happy brain in an Happy body. What more? Well, how about a little BREAK for \
+digestion purposes? (or BACK to work.... Really??)",
+    "after break": "Ouh, time passes so fast when your checking for new shoes on amazon! Fine, let's go BACK to coding, \
+but not before you pass by the W.C., check your hair and your teeth in the mirror, put on a warm shirt, make yourself \
+another TEA, grab some CHOCOLATE...",
+    "call_friend": "After a couple of calls, someone from the inner circle picks up, laughs at you a little and says that he will send you the much needed \
+lines of code through GitHUb. Smart move buddy, you congratulate yourself for not wasting time and/or not learning much...",
+    "chatgpt": "Takes a couple of tweeking in the prompting but, as intended, the Truth appears before your eyes: pure code, \
+devoid of humanity and predictable variable names. The code is unreadable but, who are you to stand in the middle of computers \
+helping each other so efficiently? Smart move. Just don't tell anyone what you've done and earn yourself more time to spend on more entertaining stuff! \
+Life is not that hard right? Will you ever CODE again, QUIT this nonsensical absurdity of a mini game, or check updates \
+on your coder's PROFILE?",
+    "frustration": "HOOOOOOLLLY COW!!! Computers are so incredibly AUTISTIC! You realize at this instant how much you were not \
+made to work in a mental institution...but you might become a patient if you let all this anger control yourself! \
+You take a second to contemplate this issue: and decide to come BACK and evaluate this situation calmly, or, turn to \
+the DARK side and express some of that accumulated tension physically?",
+    "dark": "Something has got to give and various options are rapidly presenting themselves: you can SCREAM in a pillow, let a very long and very loud F-WORD out, \
+SLAP something on the desk or just PUNCH that wall with all you've got...",
+    "pillow": "After this mini crisis episode, you go and refresh your face in the bathroom. Once the mirror-check \
+reflects your normal 'OK' face again, you come back to your post, take a deep breath and start to CODE again...",
+    "cursing": "WAouuuuuh that felt good! Sometimes, cursing is all you've got...for now, you're 'OK' (are you? want to 'STOP'?) but you can't shake the feeling that \
+you might have been noticed by someone else, and that, makes you want to curse even more. Will you risk another very loud 'FUUU' ??",
     "study": "Zen as a bonsai, you turn to other websites and try not to deviate from the path too much...",
-    "study2": "You take a couple of minutes to ponder your options: you can always let it go and SLEEP ON IT, \
+    "study2": "You take a couple of minutes to ponder your options: you can always let it go and SLEEP on it, \
 see what tomorrow brings. Also you can OBSESS and stay on course until you make the code work or, \
 look inward and see if you are not, in fact, building some kind of FRUSTRATION",
-    "obsess": "OK, you got this. Knife between your teeth you try to make it work using the sheer power of Will",
-    "abandon": "y",
-    "x": "y",
-    "x": "y",
-    "x": "y",
-    "x": "y",
-    "x": "y",
-    "x": "y",
-    "x": "y",
-    "profile": "y",
-    "success": "You did it! WOW!.... ....does it matter how? Let's check your PROFILE together??",
-
-  
+    "obsess": "OK, you got this. Knife between your teeth you try to make it work using the sheer power of your Will",
+    "abandon": "You are failing and you know it. There most be a simple explanation, but it is out of reach for the moment. \
+Time to lick you wounds, admit defeat and gather your strengh for the next fight. Another day, another code to write....what will you do? \
+START AGAIN, meditate on your coder's PROFILE or just QUIT?",
+    "sleep": "Time to get your 'Mojo' back, meditate, and experience the cycle of life again. Breathing in and out. This is no lazyness (or is it?). \
+This is how the experienced thinker acts: trusting one's brain to classify all the data during its 'reboot phase' and eventually reveal the missing links...",
+    "nap success": "What a difference a good nap makes! You haven't touched the keyboard and yet, you are already \
+formulating solutions within a smile. Your refreshed self is winning, the rest bears its fruit. You feel wiser and a little more proficient at coding. How about that?\
+ Hopefully it will work NEXT TIME. Or maybe you would like to check updates on your coder's PROFILE?",
+    "punch": "You turn to that ugly wall next to the desk and hit it with your right fist. AOUCH!... Damn, \
+you have hit precisely where the supporting beam was! Your hand hurts a little and the wall is somehow mocking you. \
+What does this experience teaches you? That you need to 'CALM DOWN', or go the toolbox, get the HAMMER and bring that \
+stupid wall down?",
+    "strangle duck": "You put your hands around the neck of the duck. You start squeezing until you hear a \
+'Kwaaaakkk'. You can't help but smile as you see yourself a rubber duck murderer. You squeeze so more, but nothing new \
+comes out of this experience and know how childish you have become. Now, where were we? EXPLAIN some more, \
+BACK to your code or ABANDON?",
+    "slap endgame": "OUch!!! The slapping went uncontrolled and your laptop gets a big hit. Screen of Death and broken glass, \
+you are also bleeding at the wrist. What a mess!!! Seriously?? This couldn't have gone worse. \
+You are dangerous. This mini game is booting you out as to avoid further material destruction. Good luck with your temper issues Pal!",
+    "angry endgame": "With so much noise, someone calls the autorities on you. You get a serious warning and find yourself in such a state of \
+anger and despair that you are rendered inoperable. You need to CALM DOWN! Seriously. \
+This mini game is booting you out as to avoid further material destruction. Have a nice Life Buddy!",
+    "autism": "Autism, also called autism spectrum disorder(ASD) or autism spectrum condition (ASC), is a neurodevelopmental disorder \
+marked by restricted and repetitive patterns of behavior and deficits in reciprocal social communication. Other common signs include perseverative \
+interests, stereotypic body movements (stimming), rigid routines, hyper- or hyporeactivity to sensory input, and difficulty with social interaction \
+and verbal and nonverbal communication. (type 'BACK' exactly, without any mistake or I will not understand!)",
+    "success": "OH! You did it! WOW! Impressive. You do a little dance...and just between us: you know it's 3 a.m. right? \
+Did you missed that call from your mother? You will probably be coding in bed also, good luck sleeping! \
+START again because you are a Maniac or check updates on your coder's PROFILE together?",
+    "duck success": "Incredible! Confessional Programming IS an actual working method for debugging and you are now \
+living proof of it. Going back to the code, you find that hidden bug and make everything running smoothly again. You feel smart \
+and adequate. TODO: Celebrate fully and check updates on your coder's PROFILE or on to the NEXT challenge??",
 }
  
 solutions = {
-    "start": {"write code": "first options"},
-    "first options": {"stack overflow": "stack overflow", "duck": "duck", "fridge": "fridge", "friend": "friend"},
-    "stack overflow": {"frustration": "frustration", "friend": "friend"},
-    "study": {"duck": "duck", "fridge": "fridge", "frustration": "frustration"},
-    "friend": {"write code": "first options", "z": "z1", "a": "a1"},
-    "frustration": {"y": "y1", "z": "z1", "a": "a1"},
-    "obsess": {"abandon": "abandon", "s": "s", "a": "a1"},
-    "abandon": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "x": {"y": "y1", "z": "z1", "a": "a1"},
-    "profile": {"y": "y1", "z": "z1", "a": "a1"},
-    "sucess": {"profile": "profile", "z": "z1", "a": "a1"},
+    "start": {"write code": "first options", "fru": "frustration", "code": "first options"},
+    "first options": {"google": "stack overflow", "stack overflow": "stack overflow", "stack": "stack overflow", "duck": "duck", "kitchen": "kitchen"},
+    "stack overflow": {"frustration": "frustration", "friend": "call_friend"},
+    "study": {"duck": "duck", "kitchen": "kitchen", "frustration": "frustration"},
+    "friend": {"start": "start"},
+    "chatgpt": {"code": "start"},
+    "frustration": {"autistic": "autism", "back": "first options", "dark": "dark"},
+    "dark": {"punch": "punch"},
+    "autism": {"back": "frustration"},
+    "cursing": {"ok": "start", "stop": "start", "better": "start", "write code": "first options"},
+    "obsess": {"abandon": "abandon", "profile": "profile", "start": "start"},
+    "abandon": {"start again": "start", "profile": "profile"},
+    "sleep": {"write code": "first options", "next time": "start", "abandon": "abandon", "profile": "profile"},
+    "pillow": {"code": "first options", "ok": "first options"},
+    "f-word": {"write code": "first options"},
+    "slap": {"write code": "first options","dark": "dark", "back": "start", "coding": "first options"},
+    "punch": {"write code": "first options", "better": "start", "calm down": "first options", "dark": "dark"},
+    "duck": {"rubber ducking": "rubber ducking", "options": "first options", "strangle": "strangle duck", "start": "start"},
+    "rubber ducking": {"back": "duck"},
+    "strangle duck": {"back": "first options", "abandon": "abandon"},
+    "kitchen": {"back": "first options"},
+    "duck success": {"next": "start", "profile" : "profile"},
+    "profile": {"coding": "start"},
+    "sucess": {"profile": "profile", "start": "start"},
 }
 
 analisis ={
-    "obsessed":"Very confident in your own capacities. But, you are tired, exhausted...and after a while, so is everyone around you. Not sure you can do the same tomorrow...\
-It kinds of beg the question: Wouldn't it be more productive to take a breather sometimes?"
-
-
+    "zen":"Your definition of Zen is borderline between 'My body is a Temple' and 'fulfilling every little \
+bodily impulses of mine is prior to...'. As Bobby said: it is your prerogative. And after all, we don't want you complaining, or \
+stressed, unhappy, sick, hungry...it's not good for business! However, you also need a roof over your head. \
+Have you done any work today?",
+    "angry":"Terrible! Intimidating. A constant menace to collegues and any sensitive electrical devices around you \
+when you are coding. Anger issues? We all have them, especially driving during rush hour. Someday, you will realize that \
+we are all angry inside. Some are better at managing/hiding it, that's all. Now, careful next time you spin out of \
+control: you might open the cage for an angrier beast... and/or just get a bad rep!",
+    "smart":"It is not enough to have the fastest bike, you need to keep it in functioning order and tune it to your \
+preferences. Well, that bike is you brain and you are well aware of it. There's nothing we can tell you here \
+that you don't already know. Keep on making the right decision and soon you will see us mortals miles away from your \
+rearview mirror",
+    "obsessed":"You are definitely very confident in your own capacities. But, you end up sooooo tired, soooo exhausted...\
+and after a while, so is everyone around you. Not sure you can keep this up for long. How about tomorrow? \
+It kinds of beg the question: Wouldn't it be more productive to take a breather sometimes?",
+    "dependent": "Asking for help is fine and respecting deadlines is fundamental. Good point. Hopefully, you are aware that \
+abusing the people around you is not a viable carreer plan. Are people starting to avoid your calls or talk behind your back?\
+ Also, how's your coding going? Any notable progress lately?",
 }
